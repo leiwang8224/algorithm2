137aba32c2e35b62adaa2561590402334e5985b4
Lei Wang <leiwang8224@gmail.com>
2019-05-15 17:21:56 -0700

    Added some stuff to DP and Math

--- src/DFS/BoggleSearch.java
+++ src/DFS/BoggleSearch.java
@@ -1,5 +1,7 @@
 package DFS;
 
+import java.util.Arrays;
+
 public class BoggleSearch {
     public static void main(String[] args) {
         char[][] board = {{'a','b','c','d','e'},
@@ -7,7 +9,7 @@ public class BoggleSearch {
                         {'a','s','e','f','e'},
                         {'a','e','e','f','a'},
                         {'a','e','e','f','a'}};
-        System.out.println(boggleSearch(board, "base"));
+        System.out.println(boggleSearch(board, "acdffee"));
 
 
     }
@@ -34,6 +36,11 @@ public class BoggleSearch {
                                   String predecessor) {
         int rows = board.length;
         int cols = board[0].length;
+        System.out.println(predecessor + " row = " + row + " col = " + col);
+        for (char[] line : board) {
+            System.out.println(Arrays.toString(line));
+        }
+        System.out.println("end");
 
         if (row > rows-1 ||
             row < 0 ||
@@ -49,11 +56,14 @@ public class BoggleSearch {
         boolean out = false;
         if (s.equals(word)) return true;
         else {
+            // not equals to word so set position as searched
             board[row][col] = '@';
+            // search top, bottom, left and right (backtracking)
             out = search(row-1, col, board, word, s)
                     || search (row+1, col, board, word, s)
                     || search (row,col-1, board, word, s)
                     || search (row, col+1, board, word, s);
+            // set char to the board position
             board[row][col] = ch;
         }
         return out;
