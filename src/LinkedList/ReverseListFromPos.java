77a1068ced82a79e1d71b0c46e0d7b28e1c177cd
Lei Wang <leiwang8224@gmail.com>
2019-08-16 13:19:13 -0700

    Added some stuff to DP and Math

--- src/LinkedList/ReverseListFromPos.java
+++ src/LinkedList/ReverseListFromPos.java
@@ -10,24 +10,29 @@ public class ReverseListFromPos {
         if (head == null) return null;
         ListNode dummy = new ListNode(0);
         dummy.next = head;
-        ListNode pre = dummy;
+        ListNode beforeStart = dummy;
+
+        // traverse to the position right before the start position
         for (int i = 0; i < listStartReverse-1; i++) {
-            pre = pre.next;
+            beforeStart = beforeStart.next;
         }
 
         // pointer to the beginning of a sub-list that will be reversed
-        ListNode start = pre.next;
+        ListNode start = beforeStart.next;
 
         // pointer to a node that will be reversed
-        ListNode then = start.next;
+        ListNode nextOfStart = start.next;
 
-        // 1 - 2 -3 - 4 - 5 ; m=2; n =4 ---> pre = 1, start = 2, then = 3
-        // dummy-> 1 -> 2 -> 3 -> 4 -> 5
-        for (int i = 0; i < listEndReverse - listStartReverse; i++) {
-            start.next = then.next;
-            then.next = pre.next;
-            pre.next = then;
-            then = start.next;
+        // calculate the length for reversal
+        int lengthForReverse = listEndReverse - listStartReverse;
+
+        // 1 - 2 - 3 - 4 - 5 ; start=2; end=4 ---> pre = 1, start = 2, then = 3
+        // dummy -> 1 -> 2 -> 3 -> 4 -> 5
+        for (int i = 0; i < lengthForReverse; i++) {
+            start.next = nextOfStart.next;
+            nextOfStart.next = beforeStart.next;
+            beforeStart.next = nextOfStart;
+            nextOfStart = start.next;
         }
 
         // first reversing : dummy->1 - 3 - 2 - 4 - 5; pre = 1, start = 2, then = 4
